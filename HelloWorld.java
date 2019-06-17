43bf89a3b5d33e3a4ce27cee350ba3be367daed6
eikichi0119 <vickinia1018@gmail.com>
2019-06-17 16:37:02 +0800

    Sixth line removed.

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,5 @@ public class HelloWorld {
 		System.out.println("This is the third line added.");
 		System.out.println("This is the fourth line added.");
 		System.out.println("This is the fifth line added.");
-		System.out.println("This is the sixth line added.");
 	}
 }
